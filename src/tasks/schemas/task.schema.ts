3782e4a35776f38f2367750671c9501c1027aa47
Cespedes Brian <briancdevelop@gmail.com>
2026-07-19 10:34:20 -0300

    feat: add task schema

--- src/tasks/schemas/task.schema.ts
+++ src/tasks/schemas/task.schema.ts
@@ -1,1 +1,31 @@
-export class Task {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { TaskStatus } from "../enums/task-status.enum";
+
+export type TaskDocument = HydratedDocument<Task>
+
+@Schema({
+  timestamps: true,
+})
+export class Task {
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  title!: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  description!: string;
+
+  @Prop({
+    required: true,
+    enum: TaskStatus,
+    default: TaskStatus.PENDING,
+  })
+  status!: TaskStatus;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task)
