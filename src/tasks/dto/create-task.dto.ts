055f2f2912dc1cee4495e83bc225d5620e3d4469
Cespedes Brian <briancdevelop@gmail.com>
2026-07-19 10:53:22 -0300

    feat: add task validation DTOs

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,18 @@
-export class CreateTaskDto {}
+import { IsEnum, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { TaskStatus } from '../enums/task-status.enum';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(100)
+  title!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(500)
+  description!: string;
+
+  @IsEnum(TaskStatus)
+  status!: TaskStatus;
+}
